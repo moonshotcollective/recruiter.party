0f653b4adafe431fd50573178b536faebc7de057
qedk <1994constant@gmail.com>
2021-12-24 13:14:37 +0530

    feat: Bump contracts to 0.8.11, remove extra settings

--- packages/hardhat-ts/contracts/DRecruitV1.sol
+++ packages/hardhat-ts/contracts/DRecruitV1.sol
@@ -31,7 +31,7 @@
         / /___/ /_/ / /___/ /___/ /___/ /___  / / _/ /  | |/ / /___
         \____/\____/_____/_____/_____/\____/ /_/ /___/  |___/_____/
 */
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
--- packages/hardhat-ts/hardhat.config.ts
+++ packages/hardhat-ts/hardhat.config.ts
@@ -130,38 +130,11 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.10',
+        version: '0.8.11',
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200,
-          },
-        },
-      },
-      {
-        version: '0.8.6',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-      {
-        version: '0.7.6',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-      {
-        version: '0.6.7',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
+            runs: 9999,
           },
         },
       },

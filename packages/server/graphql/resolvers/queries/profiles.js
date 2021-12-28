b692c9c26d722c2fb018436aec2bc3a2ac00fe1c
qedk <1994constant@gmail.com>
2021-12-28 22:34:56 +0530

    feat: Lint code and add search field in profiles query

--- packages/server/graphql/resolvers/queries/profiles.js
+++ packages/server/graphql/resolvers/queries/profiles.js
@@ -1,30 +1,36 @@
 const { Profile } = require('../../../models/profile')
-const fastify = require('fastify')({ logger: true })
 
 const QueryProfiles = {
-	profiles: async (_, obj) => {
-		const { limit, skip, name, residenceCountry, skills } = obj;
-		fastify.log.info("Profiles Query")
-		let filters = {}
-		if (name) {
-			filters["name"] = new RegExp(name, 'i')
-		}
-		if (residenceCountry) {
-			filters["residenceCountry"] = residenceCountry
-		}
-		if (skills) {
-			filters["skills"] = { $all: skills }
-		}
-		const options = { limit: limit, skip: skip }
-		const profiles = await Profile.find(filters, null, options);
-		return profiles;
-	},
+  profiles: async (_, obj) => {
+    const { limit, skip, name, residenceCountry, skills, search } = obj
+    const filters = {}
+    const sort = {}
+    const options = { limit, skip }
+    if (name) {
+      filters.name = new RegExp(name, 'i')
+    }
+    if (residenceCountry) {
+      filters.residenceCountry = residenceCountry
+    }
+    if (skills) {
+      filters.skills = { $all: skills }
+    }
+    if (search) {
+      filters.$text = { $search: search }
+      filters.score = { $meta: 'textScore' }
+      sort.score = { $meta: 'textScore' }
+    }
+    console.log(filters, sort, options)
+    const profiles = await Profile.find(filters, sort, options).lean()
+    console.log(profiles)
+    return profiles
+  },
 
-	profile: async (_, obj) => {
-		const { tokenId } = obj;
-		const profile = await Profile.findOne({ tokenId: tokenId});
-		return profile;
-	}
+  profile: async (_, obj) => {
+    const { tokenId } = obj
+    const profile = await Profile.findOne({ tokenId: tokenId }).lean()
+    return profile
+  }
 }
 
 module.exports = { QueryProfiles }

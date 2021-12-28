38f824ac392ae1c0053cb0596a401d68ccb316cb
qedk <1994constant@gmail.com>
2021-12-28 22:33:06 +0530

    feat: Fix rules and lint

--- packages/server/.eslintrc.js
+++ packages/server/.eslintrc.js
@@ -1,24 +1,12 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
   env: {
-    node: true,
-    jest: true,
+    browser: false,
+    commonjs: true,
+    es2021: true,
+    node: true
   },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
-};
+  extends: ['standard', 'plugin:node/recommended', 'plugin:import/recommended', 'eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 12
+  }
+}

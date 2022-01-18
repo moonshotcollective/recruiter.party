95cefcf7cd21c15979af9140f05fc6cd28102c60
Damian <damianmarti@gmail.com>
2022-01-17 23:26:51 -0300

    Remove Basic Profile

--- packages/schemas/src/schemas.ts
+++ packages/schemas/src/schemas.ts
@@ -1,13 +1,11 @@
 import {
   PrivateProfileSchema,
-  BasicProfileSchema,
   PublicProfileSchema,
 } from "./profiles/profiles-schema";
 
 export const schemas = {
   dapp: {
     PrivateProfile: PrivateProfileSchema,
-    BasicProfile: BasicProfileSchema,
     PublicProfile: PublicProfileSchema,
   },
 };

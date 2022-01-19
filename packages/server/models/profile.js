626ec95a7be498eb9763305c6e447ba5a874386f
Damian <damianmarti@gmail.com>
2022-01-19 18:34:12 -0300

    Script to create new profiles on MongoDB

--- packages/server/models/profile.js
+++ packages/server/models/profile.js
@@ -1,43 +1,125 @@
 const mongoose = require('mongoose')
 
+const imageSources = {
+  original: {
+    src: {
+      type: String
+    },
+    size: {
+      type: Number
+    },
+    width: {
+      type: Number
+    },
+    height: {
+      type: Number
+    },
+    mimeType: {
+      type: String
+    }
+  }
+}
+
+const experienceSchema = new mongoose.Schema({
+  company: {
+    type: String
+  },
+  title: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  startDate: {
+    type: Date
+  },
+  endDate: {
+    type: Date
+  }
+})
+
+const educationSchema = new mongoose.Schema({
+  institution: {
+    type: String
+  },
+  title: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  startDate: {
+    type: Date
+  },
+  endDate: {
+    type: Date
+  }
+})
+
+const basicProfile = new mongoose.Schema({
+  url: {
+    type: String
+  },
+  name: {
+    type: String
+  },
+  emoji: {
+    type: String
+  },
+  image: imageSources,
+  gender: {
+    type: String
+  },
+  birthDate: {
+    type: String
+  },
+  background: {
+    type: imageSources
+  },
+  description: {
+    type: String
+  },
+  affiliations: {
+    type: [String]
+  },
+  homeLocation: {
+    type: String
+  },
+  nationalities: {
+    type: [String]
+  },
+  residenceCountry: {
+    type: String
+  }
+})
+
+const publicProfile = new mongoose.Schema({
+  skillsTags: {
+    type: [String]
+  },
+  experience: {
+    type: [experienceSchema]
+  },
+  education: {
+    type: [educationSchema]
+  }
+})
+
 const profileSchema = new mongoose.Schema({
   tokenId: {
     type: Number,
     unique: true,
     required: true
   },
-  name: {
-    type: String
+  did: {
+    type: String,
+    required: true
   },
-  description: {
-    type: String
+  basicProfile: {
+    type: basicProfile
   },
-  birthDate: {
-    type: String
-  },
-  gender: {
-    type: String
-  },
-  residenceCity: {
-    type: String
-  },
-  residenceCountry: {
-    type: String
-  },
-  nationalities: {
-    type: [String]
-  },
-  affiliations: {
-    type: [String]
-  },
-  education: {
-    type: [String]
-  },
-  skills: {
-    type: [String]
-  },
-  experience: {
-    type: [String]
+  publicProfile: {
+    type: publicProfile
   }
 })
 

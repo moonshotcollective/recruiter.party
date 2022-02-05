4fad0251c252cf7b6c1b248273ebfe291871372f
Damian <damianmarti@gmail.com>
2022-02-05 11:52:17 -0300

    Add more profile search fields on graphQL

--- packages/server/graphql/resolvers/queries/profiles.js
+++ packages/server/graphql/resolvers/queries/profiles.js
@@ -2,18 +2,33 @@ const { Profile } = require('../../../models/profile')
 
 const QueryProfiles = {
   profiles: async (_, obj) => {
-    const { limit, skip, name, residenceCountry, skills, search } = obj
+    const { limit, skip, name, residenceCountry, homeLocation, educationInstitution, educationTitle, experienceCompany, experienceTitle, skills, search } = obj
     const filters = {}
     const sort = {}
     const options = { limit, skip }
     if (name) {
-      filters.basicProfile.name = new RegExp(name, 'i')
+      filters['basicProfile.name'] = new RegExp(name, 'i')
     }
     if (residenceCountry) {
-      filters.basicProfile.residenceCountry = residenceCountry
+      filters['basicProfile.residenceCountry'] = residenceCountry
+    }
+    if (homeLocation) {
+      filters['basicProfile.homeLocation'] = homeLocation
     }
     if (skills) {
-      filters.publicProfile.skillTags = { $all: skills }
+      filters['publicProfile.skillTags'] = { $all: skills }
+    }
+    if (educationInstitution) {
+      filters['publicProfile.education.institution'] = new RegExp(educationInstitution, 'i')
+    }
+    if (educationTitle) {
+      filters['publicProfile.education.title'] = new RegExp(educationTitle, 'i')
+    }
+    if (experienceCompany) {
+      filters['publicProfile.experiences.company'] = new RegExp(experienceCompany, 'i')
+    }
+    if (experienceTitle) {
+      filters['publicProfile.experiences.title'] = new RegExp(experienceTitle, 'i')
     }
     if (search) {
       filters.$text = { $search: search }
